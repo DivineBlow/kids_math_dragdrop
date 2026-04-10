b938c843970766fe1e205aeb06c2206c974bb6e8
DivineBlow <sebi.26.03.97@mail.ru>
2026-04-10 14:58:57 +0300

    Optimize the compiling packages.

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    // Увеличиваем лимит предупреждения (необязательно, но полезно)
-    chunkSizeWarningLimit: 1000, 
     rollupOptions: {
       output: {
-        // Разделяем библиотеки и ваш код на разные файлы
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // Все зависимости из npm (Three.js, confetti) уйдут в отдельный файл vendor
-            return 'vendor';
-          }
+        manualChunks: {
+          three: ['three'],
+          confetti: ['canvas-confetti'],
         },
       },
     },
